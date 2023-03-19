9e01bf055c5ceb7799d38509ee68999c023988aa
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2023-03-19 22:40:08 +0530

    added some stuff

---
+++ gallery.js
@@ -0,0 +1,48 @@
+
+//add cursor functionality
+var cursor = document.getElementById("cursor");
+
+cursor_width = 16;
+document.addEventListener("mousemove",function(e){
+        x=e.clientX-cursor_width;
+        y=e.clientY-cursor_width;
+    cursor.style.transform = " translate3d("+x+"px,"+y+"px,0)";
+});
+
+/*dark mode*/
+var body=document.getElementsByTagName("body")[0];
+
+var bg_ball1=document.getElementById("bg-ball1");
+var bg_ball2=document.getElementById("bg-ball2");
+var cursor_dot=document.getElementById("cursor-dot");
+var tablinks=document.getElementsByClassName("tablinks");
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    // dark mode 
+    body.classList.add("body-dark");
+    bg_ball1.classList.add("bg-ball1-dark");
+    bg_ball2.classList.add("bg-ball2-dark");
+    cursor.classList.add("cursor-dark");
+    cursor_dot.classList.add("cursor-dot-dark");
+    for(i=0;i<tablinks.length;i++){
+        tablinks[i].classList.add("tablinks-dark");
+    }
+    //dark mode image change
+    dark_state=true;
+  
+}
+
+/*tablinks*/
+function openTab(evt, tabName){
+    var tab_open= document.getElementById(tabName);
+    var tablinks=document.getElementsByClassName("tablinks");
+    for(i=0;i<tablinks.length;i++){
+        tablinks[i].classList.remove("active");
+    }
+    /*turn all other tabs off*/
+    var tabcontent=document.getElementsByClassName("tabcontent");
+    for(i=0;i<tabcontent.length;i++){
+        tabcontent[i].style.display="none";
+    }
+    tab_open.style.display="block";
+    evt.currentTarget.classList.add("active");
+}
